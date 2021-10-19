905e00e9f81cd4022ce521364bd646247ffeaa5e
yhsol <loshY244110@gmail.com>
2021-10-19 17:32:55 +0900

    feat: Extract Variable

--- learn/book/리팩터링2판/chapter6.ts
+++ learn/book/리팩터링2판/chapter6.ts
@@ -47,3 +47,55 @@ function reportLines(aCustomer) {
   lines.push(["location", aCustomer.location]);
   return lines;
 }
+
+function extractVariable() {
+  let order = {
+    quantity: 0,
+    itemPrice: 0,
+  };
+
+  // before
+  // return (
+  //   order.quantity * order.itemPrice -
+  //   Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+  //   Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  // );
+
+  // refactoring
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice + 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
+}
+
+class Order {
+  _data: {
+    quantity: number;
+    itemPrice: number;
+  };
+
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+}
