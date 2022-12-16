97b3cd3c2ad46b1e4f608e6b3b44e5796421170a
yhsol <loshy244110@gmail.com>
2022-12-17 00:39:38 +0900

    feat: __proto__

---
+++ note/learn/javascript/JavaScript Object Oriented Programming/__proto__.js
@@ -0,0 +1,9 @@
+var superObj = { superVal: "super" };
+var subObj = { subVal: "sub" };
+subObj.__proto__ = superObj;
+console.log("subObj.subVal =>", subObj.subVal);
+console.log("subObj.superVal =>", subObj.superVal);
+//=> js -> super
+//=> ts -> undefined
+subObj.superVal = "sub";
+console.log("superObj.superVal = >", superObj.superVal);
